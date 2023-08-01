dfd8b2e0ca5196ae7d09eff038ffc4c907dd388b
Vera Tsiribko <vera.korchemnaya@intellitect.com>
2023-08-01 10:29:42 -0700

    Full width and hieght

--- index.js
+++ index.js
@@ -3,26 +3,29 @@ const { Engine, Render, World, Bodies, Composite, Runner } = Matter;
 
 // create an engine
 var engine = Engine.create();
+const canvas = document.getElementById('canvas');
+const canvasWidth = canvas.offsetWidth;
+const canvasHeight = canvas.offsetHeight;
 
-// create a renderer
 var render = Render.create({
-    element: document.body,
-    engine: engine
+    element: canvas,
+    engine: engine,
+    options: {
+        width: canvas.offsetWidth,
+        height: canvas.offsetHeight,
+        wireframes: false
+    }
 });
 
 // create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 1);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
+var boxA = Bodies.rectangle(200, 200, 80, 20);
+var boxB = Bodies.rectangle(250, 50, 80, 80);
 var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 // add all of the bodies to the world
 Composite.add(engine.world, [boxA, boxB, ground]);
 
-// run the renderer
+
 Render.run(render);
-
-// create runner
 var runner = Runner.create();
-
-// run the engine
 Runner.run(runner, engine);
